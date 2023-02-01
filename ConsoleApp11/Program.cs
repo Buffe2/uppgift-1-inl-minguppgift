cbc58cf1e766c532229414ce2bf2d2b9b78ce316
GustavJohansson420 <gujo04002@utb.vaxjo.se>
2023-02-01 10:00:54 +0100

    Add project files.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Uppgift_2._1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilken är din favoritbok?");
+            string strNr1 = Console.ReadLine();
+            Console.WriteLine("Din favoritbok är... "+ strNr1);
+        }
+    }
+}
+
